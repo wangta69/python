6722be8e50895f89a010bfe57051cd1af2c2ebdb
younghyeong ryu <wangta69@naver.com>
2021-06-23 18:55:09 +0900

    t
    t

--- kiwoom/main-test.py
+++ kiwoom/main-test.py
@@ -1,5 +1,5 @@
 import sys
-# from kiwoom.login import *
+from kiwoom.login import *
 # from kiwoom.reqestThread import *
 from PyQt5.QtWidgets import *
 from PyQt5.QAxContainer import QAxWidget
@@ -14,11 +14,11 @@ class Main():
         print("메인 클래스입니다.")
 
         # 요청 쓰레드
-        self.request_thread_worker = RequestThreadWorker()
-        self.request_thread = QThread()
-        self.request_thread_worker.moveToThread(self.request_thread)
-        self.request_thread.started.connect(self.request_thread_worker.run)
-        self.request_thread.start()
+        # self.request_thread_worker = RequestThreadWorker()
+        # self.request_thread = QThread()
+        # self.request_thread_worker.moveToThread(self.request_thread)
+        # self.request_thread.started.connect(self.request_thread_worker.run)
+        # self.request_thread.start()
 
         self.app = QApplication(sys.argv) # QApplication 객체 생성.
         self.login = Klogin()
@@ -29,22 +29,22 @@ if __name__ == "__main__":
     # 로거 (Logger) 준비하기
     # --------------------------------------------------
     # 로그 파일 핸들러
-    fh_log = TimedRotatingFileHandler('logs/log', when='midnight', encoding='utf-8', backupCount=120)
-    fh_log.setLevel(logging.DEBUG)
-
-    # 콘솔 핸들러
-    sh = logging.StreamHandler()
-    sh.setLevel(logging.DEBUG)
-
-    # 로깅 포멧 설정
-    formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
-    fh_log.setFormatter(formatter)
-    sh.setFormatter(formatter)
-
-    # 로거 생성 및 핸들러 등록
-    logger = logging.getLogger(__file__)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(fh_log)
-    logger.addHandler(sh)
+    # fh_log = TimedRotatingFileHandler('logs/log', when='midnight', encoding='utf-8', backupCount=120)
+    # fh_log.setLevel(logging.DEBUG)
+    #
+    # # 콘솔 핸들러
+    # sh = logging.StreamHandler()
+    # sh.setLevel(logging.DEBUG)
+    #
+    # # 로깅 포멧 설정
+    # formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
+    # fh_log.setFormatter(formatter)
+    # sh.setFormatter(formatter)
+    #
+    # # 로거 생성 및 핸들러 등록
+    # logger = logging.getLogger(__file__)
+    # logger.setLevel(logging.DEBUG)
+    # logger.addHandler(fh_log)
+    # logger.addHandler(sh)
 
     Main()
--- kiwoom/main.py
+++ kiwoom/main.py
@@ -1,9 +1,52 @@
 import sys
 import logging
+import time
 from logging.handlers import TimedRotatingFileHandler
 from PyQt5.QAxContainer import QAxWidget
 from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QThread
 from PyQt5.QtWidgets import QApplication
+from collections import deque
+from threading import Lock
+
+class RequestThreadWorker(QObject):
+    def __init__(self):
+        """요청 쓰레드
+        """
+        super().__init__()
+        self.request_queue = deque()  # 요청 큐
+        self.request_thread_lock = Lock()
+
+        # 간혹 요청에 대한 결과가 콜백으로 오지 않음
+        # 마지막 요청을 저장해 뒀다가 일정 시간이 지나도 결과가 안오면 재요청
+        # self.retry_timer = None
+        self.retry_timer = 50
+
+    def retry(self, request):
+        logger.debug("키움 함수 재시도: %s %s %s" % (request[0].__name__, request[1], request[2]))
+        self.request_queue.appendleft(request)
+
+    def run(self):
+        while True:
+            # 큐에 요청이 있으면 하나 뺌
+            # 없으면 블락상태로 있음
+            try:
+                request = self.request_queue.popleft()
+            except IndexError as e:
+                time.sleep(self.retry_timer)
+                continue
+
+            # 요청 실행
+            logger.debug("키움 함수 실행: %s %s %s" % (request[0].__name__, request[1], request[2]))
+            request[0](trader, *request[1], **request[2])
+
+            # 요청에대한 결과 대기
+            if not self.request_thread_lock.acquire(blocking=True, timeout=5):
+                # 요청 실패
+                time.sleep(self.retry_timer)
+                self.retry(request)  # 실패한 요청 재시도
+
+            time.sleep(self.retry_timer)  # 0.2초 이상 대기 후 마무리
 
 class SyncRequestDecorator:
     '''키움 API 동기화 데코레이터
@@ -24,11 +67,28 @@ class SyncRequestDecorator:
         return func_wrapper
 
 class SysTrader(QObject):
-    def __init__():
-        """자동투자시스템 메인 클래스
-        """
+    def __init__(self):
+        super().__init__()
+        # 자동투자시스템 메인 클래스
         self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.kiwoom.OnEventConnect.connect(self.kiwoom_OnEventConnect)  # 로그인 결과를 받을 콜백함수 연결
+        self.kiwoom.OnReceiveTrData.connect(self.kiwoom_OnReceiveTrData)
+
+        # self.kiwoom_CommConnect()  # 로그인
+        # 요청 쓰레드
+        self.request_thread_worker = RequestThreadWorker()
+        self.request_thread = QThread()
+        self.request_thread_worker.moveToThread(self.request_thread)
+        self.request_thread.started.connect(self.request_thread_worker.run)
+        self.request_thread.start()
+
+    @SyncRequestDecorator.kiwoom_sync_request
+    def kiwoom_REQUESTNAME(self, x):
+        pass
+
+    @SyncRequestDecorator.kiwoom_sync_callback
+    def kiwoom_OnCALLBACKNAME(self, x):
+        pass
 
     # -------------------------------------
     # 로그인 관련함수
@@ -60,6 +120,96 @@ class SysTrader(QObject):
         elif nErrCode == 102:
             logger.debug("버전처리 실패")
 
+
+    def kiwoom_SetInputValue(self, sID, sValue):
+        """
+        :param sID:
+        :param sValue:
+        :return:
+        """
+        res = self.kiwoom.dynamicCall("SetInputValue(QString, QString)", [sID, sValue])
+        return res
+
+    def kiwoom_CommRqData(self, sRQName, sTrCode, nPrevNext, sScreenNo):
+        """
+        :param sRQName:
+        :param sTrCode:
+        :param nPrevNext:
+        :param sScreenNo:
+        :return:
+        """
+        res = self.kiwoom.dynamicCall("CommRqData(QString, QString, int, QString)",
+                                      [sRQName, sTrCode, nPrevNext, sScreenNo])
+        return res
+
+    @SyncRequestDecorator.kiwoom_sync_request
+    def kiwoom_TR_OPW00001_예수금상세현황요청(self, 계좌번호, **kwargs):
+        """계좌수익률요청
+        :param 계좌번호: 계좌번호
+        :param kwargs:
+        :return:
+        """
+        res = self.kiwoom_SetInputValue("계좌번호", 계좌번호)
+        res = self.kiwoom_CommRqData("예수금상세현황요청", "opw00001", 0, 화면번호_예수금상세현황)
+
+    @SyncRequestDecorator.kiwoom_sync_callback
+    def kiwoom_OnReceiveTrData(self, sScrNo, sRQName, sTRCode, sRecordName, sPreNext, nDataLength, sErrorCode, sMessage, sSPlmMsg, **kwargs):
+        """TR 요청에 대한 결과 수신
+        데이터 얻어오기 위해 내부에서 GetCommData() 호출
+          GetCommData(
+          BSTR strTrCode,   // TR 이름
+          BSTR strRecordName,   // 레코드이름
+          long nIndex,      // TR반복부
+          BSTR strItemName) // TR에서 얻어오려는 출력항목이름
+        :param sScrNo: 화면번호
+        :param sRQName: 사용자 구분명
+        :param sTRCode: TR이름
+        :param sRecordName: 레코드 이름
+        :param sPreNext: 연속조회 유무를 판단하는 값 0: 연속(추가조회)데이터 없음, 2:연속(추가조회) 데이터 있음
+        :param nDataLength: 사용안함
+        :param sErrorCode: 사용안함
+        :param sMessage: 사용안함
+        :param sSPlmMsg: 사용안함
+        :param kwargs:
+        :return:
+        """
+
+        if sRQName == "예수금상세현황요청":
+            self.int_주문가능금액 = int(self.kiwoom_GetCommData(sTRCode, sRQName, 0, "주문가능금액"))
+            logger.debug("주문가능금액: %s" % (self.int_주문가능금액,))
+
+
+ # -------------------------------------
+    # 조건검색 관련함수
+    # GetConditionLoad(), OnReceiveConditionVer(), SendCondition(), OnReceiveRealCondition()
+    # -------------------------------------
+    @SyncRequestDecorator.kiwoom_sync_request
+    def kiwoom_GetConditionLoad(self, **kwargs):
+        """
+        조건검색의 조건목록 요청
+        :return:
+        """
+        lRet = self.kiwoom.dynamicCall("GetConditionLoad()")
+        return lRet
+
+    @SyncRequestDecorator.kiwoom_sync_callback
+    def kiwoom_OnReceiveConditionVer(self, lRet, sMsg, **kwargs):
+        """
+        조건검색의 조건목록 결과 수신
+        GetConditionNameList() 실행하여 조건목록 획득.
+        첫번째 조건 이용하여 [조건검색]SendCondition() 실행
+        :param lRet:
+        :param sMsg:
+        :param kwargs:
+        :return:
+        """
+        if lRet:
+            sRet = self.kiwoom.dynamicCall("GetConditionNameList()")
+            pairs = [idx_name.split('^') for idx_name in [cond for cond in sRet.split(';')]]
+            if len(pairs) > 0:
+                nIndex = pairs[0][0]
+                strConditionName = pairs[0][1]
+                self.kiwoom_SendCondition(strConditionName, nIndex)
 
 if __name__ == "__main__":
     # (중략)
