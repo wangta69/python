5748fc1086839bffaadb1c655ef52e5c4ce6dcaa
younghyeong ryu <wangta69@naver.com>
2021-08-10 18:56:06 +0900

    t
    t

--- kiwoom/work/quant/db-type/connMysql.py
+++ kiwoom/work/quant/db-type/connMysql.py
@@ -1,9 +1,6 @@
 import os
-import pandas as pd
 import numpy as np
 import pymysql
-import time
-import math
 from dotenv import load_dotenv
 
 #DB 테이블 칼럼대로 만든 객체
@@ -20,18 +17,6 @@ class Mysql:
     def updateFinancialStatements(self, jcode, yyyymm, dataSet):
 
         yyyymm = yyyymm.replace('/', '')
-
-        print(dataSet);
-
-        # revenue = dataSet['매출액']
-        # operating_income = dataSet['영업이익']
-        # net_income = dataSet['당기순이익']
-        # asset = dataSet['자산']
-        # liability = dataSet['부채']
-        # equity = dataSet['자본']
-        # cashflow_operating = dataSet['영업활동으로인한현금흐름']
-
-        # print('==============', ~np.isnan(revenue))
 
         revenue = dataSet['매출액'] if ~np.isnan(dataSet['매출액']) else None
         operating_income = dataSet['영업이익'] if ~np.isnan(dataSet['영업이익']) else None
@@ -85,16 +70,6 @@ class Mysql:
 
         yyyymm = yyyymm.replace('/', '')
 
-        # revenue = dataSet['매출액']
-        # operating_income = dataSet['영업이익']
-        # net_income = dataSet['당기순이익']
-        # asset = dataSet['자산']
-        # liability = dataSet['부채']
-        # equity = dataSet['자본']
-        # cashflow_operating = dataSet['영업활동으로인한현금흐름']
-
-        # print('==============', ~np.isnan(revenue))
-        print('update start', jcode, yyyymm, dataSet)
         current_ratio = dataSet['유동비율'] if ~np.isnan(dataSet['유동비율']) else None
         debt_ratio = dataSet['부채비율'] if ~np.isnan(dataSet['부채비율']) else None
         net_profit_margin = dataSet['영업이익률'] if ~np.isnan(dataSet['영업이익률']) else None
@@ -143,23 +118,13 @@ class Mysql:
 
         yyyymm = yyyymm.replace('/', '')
 
-        # revenue = dataSet['매출액']
-        # operating_income = dataSet['영업이익']
-        # net_income = dataSet['당기순이익']
-        # asset = dataSet['자산']
-        # liability = dataSet['부채']
-        # equity = dataSet['자본']
-        # cashflow_operating = dataSet['영업활동으로인한현금흐름']
-
-        # print('==============', ~np.isnan(revenue))
-        print('update start', jcode, yyyymm, dataSet)
         per = dataSet['PER'] if ~np.isnan(dataSet['PER']) else None
         pcr = dataSet['PCR'] if ~np.isnan(dataSet['PCR']) else None
         psr = dataSet['PSR'] if ~np.isnan(dataSet['PSR']) else None
         pbr = dataSet['PBR'] if ~np.isnan(dataSet['PBR']) else None
         total_cashflow = dataSet['총현금흐름'] if ~np.isnan(dataSet['총현금흐름']) else None
 
-        print('update start', jcode, yyyymm, per, pcr, psr, pbr, total_cashflow)
+        # print('update start', jcode, yyyymm, per, pcr, psr, pbr, total_cashflow)
 
         try:
             with self.conn.cursor(pymysql.cursors.DictCursor) as curs:
@@ -196,85 +161,50 @@ class Mysql:
         finally:
             pass
 
-    #
-    # def financeinfos(self):
-    #     cur = self.conn.cursor()
-    #     sql = "SELECT id, jcode, name, q1, q2, created_at, updated_at FROM financeinnfo ORDER BY id desc"
-    #     cur.execute(sql)
-    #
-    #     rows = cur.fetchall()
-    #     return rows
-    #
-    # def updateCorporations(self, market, code, comp_name, industry, products, listed_at, sett_month, ceo, url, region):
-    #     try:
-    #         with self.conn.cursor(pymysql.cursors.DictCursor) as curs:
-    #             sql = "select id, code from corporations where code=%s limit 0, 1"
-    #             curs.execute(sql, (code))
-    #             # columns = curs.description
-    #             # print(columns)
-    #
-    #             # rs = curs.fetchall()
-    #             rs = curs.fetchone()
-    #             print(rs)
-    #
-    #             if rs == None:  # 값이 없을 경우 현재 값 입력
-    #                 print('None')
-    #                 sql = 'insert into corporations ' \
-    #                       '(market, code, comp_name, industry, products, listed_at, sett_month, ceo, url, region, created_at, updated_at) ' \
-    #                       'values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
-    #                 curs.execute(sql, (
-    #                 market, str(code), comp_name, industry, products, listed_at, sett_month, ceo, url, region,
-    #                 time.strftime('%Y-%m-%d %H:%M:%S'),
-    #                 time.strftime('%Y-%m-%d %H:%M:%S')))
-    #
-    #                 self.conn.commit()
-    #             else:
-    #                 sql = 'update corporations set '\
-    #                       'market=%s, '\
-    #                       'comp_name=%s, '\
-    #                       'industry=%s, '\
-    #                       'products=%s, '\
-    #                       'listed_at=%s, '\
-    #                       'sett_month=%s, '\
-    #                       'ceo=%s, '\
-    #                       'url=%s, '\
-    #                       'region=%s, '\
-    #                       'updated_at=%s '\
-    #                       'where id=%s'
-    #                 curs.execute(sql, (
-    #                     market, comp_name, industry, products, listed_at, sett_month, ceo, url, region, time.strftime('%Y-%m-%d %H:%M:%S'), rs['id']))
-    #                 self.conn.commit()
-    #
-    #                     # 존재할 경우 현재 값과 비교하여 동일하면 skip 하고 다를 경우 업데이트 한다.
-    #     finally:
-    #         pass
-    #
-    def corporations(self):
+
+    def updateOrder(self, yyyymm, setField, orderItem):
+
         try:
             with self.conn.cursor(pymysql.cursors.DictCursor) as curs:
-                sql = "select code, comp_name from corporations"
-                curs.execute(sql)
 
-                rs = curs.fetchall()
-                return rs
+                    sql = 'update financeinnfos target ' \
+                          'join ' \
+                          '(' \
+                          'select id, (@rownumber := @rownumber + 1) as rownum ' \
+                          'from financeinnfos ' \
+                          'cross join (select @rownumber := 0) r ' \
+                          ' where yyyymm = %s and {} is not null ' \
+                          'order by {} asc ' \
+                          ') source on target.id = source.id ' \
+                          'set {} = rownum'
+
+                    curs.execute(sql.format(orderItem, orderItem, setField), (yyyymm))
+                    self.conn.commit()
+        except:
+            print(curs._last_executed)
+            raise
         finally:
             pass
 
-    # def codeFromCompName(self, comp_name):
-    #     try:
-    #         with self.conn.cursor(pymysql.cursors.DictCursor) as curs:
-    #             # sql = "select comp_name from corporations"
-    #             # curs.execute(sql)
-    #             sql = "select code from corporations where comp_name=%s limit 0, 1"
-    #             curs.execute(sql, (comp_name))
-    #             rs = curs.fetchone()
-    #
-    #             if rs == None:  # 값이 없을 경우 현재 값 입력
-    #                 return ''
-    #             else:
-    #                 return rs['code']
-    #     finally:
-    #         pass
+    def updateScore(self, yyyymm):
+
+        try:
+            with self.conn.cursor(pymysql.cursors.DictCursor) as curs:
+
+                    sql =   'UPDATE  financeinnfos ' \
+                            'SET     score_net_income = IF(net_income > 0, 1, 0), ' \
+                            '        score_cashflow_operating = IF(cashflow_operating  > 0, 1, 0), ' \
+                            '        score_diff = IF(cashflow_operating >  net_income, 1, 0), ' \
+                            '        score_total = score_net_income +  score_cashflow_operating + score_diff ' \
+                            'WHERE   yyyymm = %s'
+
+                    curs.execute(sql, (yyyymm))
+                    self.conn.commit()
+        except:
+            print(curs._last_executed)
+            raise
+        finally:
+            pass
 
     def close(self):
         self.conn.close()
--- kiwoom/work/quant/db-type/ex2_db.py
+++ kiwoom/work/quant/db-type/ex2_db.py
@@ -1,42 +1,65 @@
 import pandas as pd
 from utils_magic import MagicUtil
-fs_path = './재무제표데이터.xlsx'
-fr_path = './재무비율데이터.xlsx'
-invest_path = './투자지표데이터.xlsx'
+from connMysql import Mysql
 
-fs_df = MagicUtil.get_finance_data(fs_path)
-fr_df = MagicUtil.get_finance_data(fr_path)
-invest_df = MagicUtil.get_finance_data(invest_path)
 
-# option
-pd.set_option('chained', None)
+class Ranking():
+    def __init__(self, parent=None):
+        super().__init__()
+        self.mysql = Mysql()
 
-# momentum
-price_path = r'D:/work/stock/quant/가격데이터.xlsx'
-#price_df = pd.read_excel(price_path, index_col=0)
 
-# 저평가 데이터프레임과 F-score 데이터프레임 만들기 (CH4. 전략 구현하기.ipynb)
-date = '2020/12'
-value = MagicUtil.make_value_combo(['PER', 'PBR', 'PSR', 'PCR'], invest_df, date, None)
-quality = MagicUtil.get_fscore(fs_df, date, None)
+    def setOrder(self):
+        '''
+        저평가된 항목을 찾기 위해 분기별 정렬을 처리한다.
+        :return:
+        '''
+        # self.mysql.updateOrder('202103', 'order_per', 'per')
+        self.mysql.updateOrder('202106', 'order_pbr', 'pbr')
+        self.mysql.updateOrder('202106', 'order_per', 'per')
+        self.mysql.updateOrder('202106', 'order_pcr', 'pcr')
+        self.mysql.updateOrder('202106', 'order_psr', 'psr')
 
-# print('value', value)
-# print('quality', quality)
+    def makescore(self):
+        '''
+        각각의 score를 매기고 종합점수를 매긴다.
+        :return:
+        '''
+        self.mysql.updateScore('202106')
+        pass
 
-# # index 주식 코드 기준 병합
-value_quality = pd.merge(value, quality, how='outer', left_index=True, right_index=True)
-# print(value_quality)
-# print(len(value_quality)) # 2263
-# # #
-value_quality.to_excel('순위All.xlsx')
-value_quality_filtered = value_quality[value_quality['종합점수'] == 3]
-# print(value_quality_filtered)
-# print(len(value_quality_filtered)) # 428
 
-vq_df = value_quality_filtered.sort_values(by='종합순위')
-# print(vq_df)
-# vq_df.to_excel('순위.xlsx')
-# print(len(vq_df)) #
+rank = Ranking()
+rank.setOrder()
+rank.makescore()
+#
+# # option
+# pd.set_option('chained', None)
+#
+#
+# # 저평가 데이터프레임과 F-score 데이터프레임 만들기 (CH4. 전략 구현하기.ipynb)
+# date = '2020/12'
+# value = MagicUtil.make_value_combo(['PER', 'PBR', 'PSR', 'PCR'], invest_df, date, None)
+# quality = MagicUtil.get_fscore(fs_df, date, None)
+#
+# # print('value', value)
+# # print('quality', quality)
+#
+# # # index 주식 코드 기준 병합
+# value_quality = pd.merge(value, quality, how='outer', left_index=True, right_index=True)
+# # print(value_quality)
+# # print(len(value_quality)) # 2263
+# # # #
+# value_quality.to_excel('순위All.xlsx')
+# value_quality_filtered = value_quality[value_quality['종합점수'] == 3]
+# # print(value_quality_filtered)
+# # print(len(value_quality_filtered)) # 428
+#
+# vq_df = value_quality_filtered.sort_values(by='종합순위')
+# # print(vq_df)
+# # vq_df.to_excel('순위.xlsx')
+# # print(len(vq_df)) #
+#
 
 
 
